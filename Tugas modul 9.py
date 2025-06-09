def542dffb7b7db74b95115fc222b8259d8850f4
rahmadefia06 <rahmadefia3003@gmail.com>
2025-06-10 01:59:24 +0700

    Add files via upload

---
+++ Tugas modul 9.py
@@ -0,0 +1,34 @@
+import os
+import time
+from termcolor import cprint, colored
+
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def tampilan_logo():
+    logo = r"""
+      (\/)    
+     (•ㅅ•)   
+     /   づ   
+    KELINCI   
+"""
+    cprint(logo, 'white', 'on_cyan', attrs=['bold'])
+
+def loading_animation():
+    loading_text = ">>> Memuat aplikasi kelinci"
+    for i in range(8):
+        dots = "." * (i % 4)
+        print(colored(loading_text + dots + "   ", 'blue'), end="\r")
+        time.sleep(1)
+    print(colored("^-^ Aplikasi siap digunakan! ^-^", 'green', attrs=['bold']))
+
+def main():
+    clear_screen()
+    cprint("="*50, 'yellow')
+    cprint("       >>> SELAMAT DATANG DI BUNNY APP <<<        ", 'red', attrs=['bold'])
+    cprint("="*50, 'yellow')
+    tampilan_logo()
+    loading_animation()
+
+if __name__ == "__main__":
+    main()
